403e074a4254920c30754138ebd42bf7108f8f1e
Ton <phantom.ness@windowslive.com>
2015-05-22 15:32:45 +0700

    install requirejs

---
+++ test-app/app/scripts/core.js
@@ -0,0 +1,6 @@
+define(function() {
+	'use strict';
+	var self = {};
+	console.log('Module ready');
+	return self;
+});
--- test-app/app/scripts/main.js
+++ test-app/app/scripts/main.js
@@ -1,1 +1,4 @@
-console.log('\'Allo \'Allo!');
+require(['core'], function(core) {
+	'user strict';
+	console.log('core module loaded', core);
+});
